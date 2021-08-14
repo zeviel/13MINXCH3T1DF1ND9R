0c76ae977eb450d61f8f2c9828a687f77386b657
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-14 12:15:26 +0600

    Update AminoChatidFinder.py

--- AminoChatidFinder.py
+++ AminoChatidFinder.py
@@ -1,4 +1,5 @@
 import amino
+import asyncio
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
@@ -7,34 +8,36 @@ print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
 print(pyfiglet.figlet_format("aminochatId", font="graffiti"))
 print(pyfiglet.figlet_format("finder", font="graffiti"))
-client = amino.Client()
-email = input("Email/Почта: ")
-password = input("Password/Пароль: ")
-client.login(email=email, password=password)
-clients = client.sub_clients(size=100)
-for x, name in enumerate(clients.name, 1):
-	print(f"{x}.{name}")
-communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
-sub_client = amino.SubClient(comId=communityid, profile=client.profile)
-print("""1.Получить chatId публичных чатов/Get public chats chatId
-2.Получить chatId чатов в которых вы находитесь/Get the chatId of the chats you are in""")
-thechatidfinder = input("Type Number/Введите цифру: ")
-if thechatidfinder == "1":
-	try:
-		print('\nLogged in and finding public chat chatids/Бот зашел и ищет chatId публичных чатов')
-		chatInfo = sub_client.get_public_chat_threads(size=1000)
-		for title, chatId in zip(chatInfo.title, chatInfo.chatId):
-		print("Public ChatIds: ")
-			print(f"{title}: {chatId}")
-	except:
-		pass
 
-elif thechatidfinder == "2":
-	try:
-		print('\nLogged in and finding chatids of the chats you are in/Бот зашел и ищет chatId чатов')
-		chatInfo = sub_client.get_chat_threads(size=1000)
-		for title, chatId in zip(chatInfo.title, chatInfo.chatId):
-		print("Chatids of the chats you are in: ")
-			print(f"{title}: {chatId}")
-	except:
-		pass
+async def main():
+	client = amino.Client()
+	email = input("Email >> ")
+	password = input("Password >> ")
+	await client.login(email=email, password=password)
+	clients = await client.sub_clients(start=0, size=100)
+	for x, name in enumerate(clients.name, 1):
+		print(f"{x}.{name}")
+	communityid = clients.comId[int(input("Select the community >> "))-1]
+	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
+	print("""[1] Get public chats chatId
+[2] Get joined chats chatId""")
+	thechatidfinder = input("Select >> ")
+	if thechatidfinder == "1":
+		try:
+			print("Finding public chats chatId")
+			chats = sub_client.get_public_chat_threads(size=100)
+			for title, chatId in zip(chats.title, chats.chatId):
+				print(f"{title} >> {chatId}")
+		except:
+			pass
+
+	elif thechatidfinder == "2":
+		try:
+			print("Finding joined chats chatId")
+			chats = sub_client.get_chat_threads(size=100)
+			for title, chatId in zip(chats.title, chats.chatId):
+				print(f"{title} >> {chatId}")
+		except:
+			pass
+
+asyncio.get_event_loop().run_until_complete(main())
