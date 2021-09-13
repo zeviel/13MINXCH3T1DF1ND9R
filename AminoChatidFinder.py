0877942bd4fc081bbc4e59ae3c0805c94bbe5e81
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-14 00:57:14 +0600

    Update AminoChatidFinder.py

--- AminoChatidFinder.py
+++ AminoChatidFinder.py
@@ -1,43 +1,38 @@
-import amino
-import asyncio
+import AminoLab
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
 print(Fore.YELLOW)
 print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
-print(pyfiglet.figlet_format("aminochatId", font="graffiti"))
-print(pyfiglet.figlet_format("finder", font="graffiti"))
+print(pyfiglet.figlet_format("aminochatidfinder", font="rectangles"))
+client = AminoLab.Client()
+email = input("Email >> ")
+password = input("Password >> ")
+client.auth(email=email, password=password)
+clients = client.my_communities()
+for x, name in enumerate(clients.name, 1):
+    print(f"{x}.{name}")
+ndc_Id = clients.ndc_Id[int(input("Select the community >> ")) - 1]
 
-async def main():
-	client = amino.Client()
-	email = input("Email >> ")
-	password = input("Password >> ")
-	await client.login(email=email, password=password)
-	clients = await client.sub_clients(start=0, size=100)
-	for x, name in enumerate(clients.name, 1):
-		print(f"{x}.{name}")
-	communityid = clients.comId[int(input("Select the community >> "))-1]
-	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
-	print("""[1] Get public chats chatId
-[2] Get joined chats chatId""")
-	thechatidfinder = input("Select >> ")
-	if thechatidfinder == "1":
-		try:
-			print("Finding public chats chatId")
-			chats = await sub_client.get_public_chat_threads(size=100)
-			for title, chatId in zip(chats.title, chats.chatId):
-				print(f"{title} >> {chatId}")
-		except:
-			pass
+print("""[1] Get Joined Chats ChatId
+[2] Get Public Chats ChatId""")
+select = input("Select >> ")
 
-	elif thechatidfinder == "2":
-		try:
-			print("Finding joined chats chatId")
-			chats = await sub_client.get_chat_threads(size=100)
-			for title, chatId in zip(chats.title, chats.chatId):
-				print(f"{title} >> {chatId}")
-		except:
-			pass
+if select == "1":
+    try:
+        joined_chats = client.my_chat_threads(ndc_Id=ndc_Id, size=100)
+        for title, thread_Id in zip(
+                joined_chats.title, joined_chats.thread_Id):
+            print(f"{title} >> {thread_Id}")
+    except Exception as e:
+        print(e)
 
-asyncio.get_event_loop().run_until_complete(main())
+elif select == "2":
+    try:
+        public_chats = client.get_public_chat_threads(ndc_Id=ndc_Id, size=100)
+        for title, thread_Id in zip(
+                public_chats.title, public_chats.thread_Id):
+            print(f"{title} >> {thread_Id}")
+    except Exception as e:
+        print(e)
